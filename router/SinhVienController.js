d49a1bbce2055811989eac66a188a12835a1693f
Admin <hieuttph27500@fpt.edu.vn>
2023-03-29 15:11:03 +0700

    update

--- router/SinhVienController.js
+++ router/SinhVienController.js
@@ -9,22 +9,70 @@ app.engine('.hbs', exphbs.engine({
     layoutsDir: "views/layouts/"
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(bodyParser.json())
 app.set('view engine', '.hbs');
 app.set('views', './views');
 app.get('/', (req, res) => {
     res.render('AddSv.hbs', { layout: 'main' });
+    console.log('http://127.0.0.1:5500/traitim.png');
 });
-app.post('/add',(req,res)=>{
+app.post('/add',async (req,res)=>{
     console.log(req.body);
-    modelSV.create({
-        ten : req.body.ten,
-        tuoi : req.body.tuoi,
-        email : req.body.email,
+    if(req.body.id == ''){
+        await modelSV.create({
+            ten : req.body.ten,
+            tuoi : req.body.tuoi,
+            email : req.body.email,
+        })
+        res.render('AddSv.hbs',{layout:'main',helpers: {
+            successfully() { return 'ADD SUCSSECCFULLY'; },
+           
+          }})
+    }
+    else{
+        
+        await modelSV.findOneAndUpdate({_id:req.body.id},req.body,{new :true},(err,doc) =>{
+            if(!err){
+                    res.redirect('/sinhVien/list')
+                
+            }
+            else{
+                console.log('Loi');
+            }
+        })
+    }
+        
+    }
+    
+)
+app.get('/edit/:id', (req, res) => {
+    modelSV.findById(req.params.id, (err, sinhVien) => {
+        if (!err) {
+            res.render('AddSv.hbs', { sinhVien: sinhVien.toJSON() })
+        }
     })
-    modelSV.find({}).then(sinhVien =>{
+})
+app.get('/delete/:id', async (req, res) => {
+    try {
+        const user = await modelSV.findByIdAndDelete(req.params.id, req.body);
+        if (!user) {
+            res.status(400).send("No item found");
+        }
+
+        else {
+            res.redirect('/sinhVien/list')
+        }
+
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+app.get('/list',async (req,res) =>{
+    await modelSV.find({}).then(sinhVien =>{
         res.render('DanhSachSv.hbs',{sinhVien : sinhVien.map(sinhViens => sinhViens.toJSON())})
     })
 })
+
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const expess = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-const { default: mongoose } = require('mongoose');
+const mongoose= require('mongoose');
 const sinhVien = require('./router/SinhVienController');
 const SinhVienModel = require('./svModel');
 
 const url = 'mongodb+srv://hieuttph27500:hieuttph27500@cluster0123.cqbgwlw.mongodb.net/dbPH27500?retryWrites=true&w=majority';
 const app = expess();
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+// app.use(bodyParser.json());
 
 app.engine('.hbs', exphbs.engine({
     extname:'hbs',
@@ -26,8 +26,8 @@ app.set('views', './views');
 
 
 
-app.use(expess.json())
-mongoose.connect(url);
+// app.use(expess.json())
+mongoose.connect(url,{useUnifiedTopology:true,useNewUrlParser:true});
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/index.html')
